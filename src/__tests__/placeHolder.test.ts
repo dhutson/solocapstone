44971a601cf3190a8610e82e1b552848913a9aba
dhutson <36569152+dhutson@users.noreply.github.com>
2021-04-01 17:28:25 -0500

    Update placeHolder.test.ts

--- src/__tests__/placeHolder.test.ts
+++ src/__tests__/placeHolder.test.ts
@@ -1,1 +1,17 @@
-// Placeholder test file
+beforeAll(() => {
+  //this would happen before ANY test in this file
+});
+
+afterAll(() => {
+  //this would happen after ALL tests in this file
+});
+
+describe("something you'll test a few times", () => {
+  beforeEach(() => {
+    //do some setup here
+  });
+  it("does x", () => {});
+  it("does y", () => {});
+});
+
+test("test is best for standalone tests", () => {});
